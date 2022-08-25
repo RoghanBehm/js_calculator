781366503c32541598c04c62c9aeb1fba6285b7e
Roghan <roghanthomasbehm@gmail.com>
2022-08-25 17:11:19 +1000

    Implement float functionality, disallow multiple operators, improve CSS design.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ let display = document.getElementById("display")
 
 let inp = "";
 let inpList = [];
+let spli;
+let spli1;
+let spli2;
 
 
 function dispInput(select) {
@@ -13,8 +16,10 @@ function dispInput(select) {
         console.log("awa");
         inp = display.textContent + " " + inpList[3] + " ";
         display.textContent = inp;
-        
-    } else display.textContent = inp;
+        return;
+    }
+    
+    display.textContent = inp;
 }
 
 let arrayInp
@@ -22,14 +27,23 @@ let arrayInp
 function makeArray() {
     arrayInp = inp.split(" ");
     operate(...arrayInp);
+    
 }
 
-function delet() {
-    if (inp.slice(-1) == " ") {
-        inp = inp.replace(/\s/g, '');
-    } 
-    inp = inp.slice(0, -1);
-    display.textContent = inp;
+function decLimit() {
+    spli = inp.split(" ");
+    spli1 = spli[0];
+    if (spli1.split(".").length == 2 && inpList.length < 2) {
+        return;
+    } if (inpList.length > 2) {
+        spli2 = spli[2];
+        if (spli2.split(".").length == 2) {
+            return;
+        }
+
+    }
+    dispInput(".");
+}
 
 one.onclick = () => dispInput("1");
 two.onclick = () =>  dispInput("2");
@@ -45,8 +59,11 @@ plus.onclick = () => dispInput(" + ");
 minus.onclick = () => dispInput(" - ");   
 divi.onclick = () => dispInput(" / "); 
 multi.onclick = () => dispInput(" * ");
-power.onclick = () => dispInput(" ^ ");
-point.onclick = () => dispInput(".");
+exponent.onclick = () => dispInput(" ** ");
+point.addEventListener("click", () => {
+    decLimit();
+})
+    
 equals.addEventListener("click", () => {
     makeArray();
 })
@@ -56,7 +73,14 @@ clear.addEventListener("click", () => {
     display.textContent = inp;
 })
 
-
+function delet() {
+    if (inp.slice(-1) == " ") {
+        inp = inp.replace(/\s/g, '');
+    } 
+    inp = inp.slice(0, -1);
+    display.textContent = inp;
+    inpList = inp.split(" ");
+    inpList = inpList.filter((a) => a);
 }
 del.addEventListener("click", () => {
     delet();
@@ -86,56 +110,128 @@ window.addEventListener("keydown", function(e){
         dispInput(" / ");
         return;
     } if (e.key == ".") {
-        dispInput(e.key);
+        decLimit();
+    } if (e.key == "^") {
+        dispInput(" ** ");
     }
  
     
 })
+    
+
 
 
 function operate(val1, operator, val2) {
     if (operator == "*") {
         multiply(val1, val2);
     } else if (operator == "/") {
+        if (val1 == 0 || val2 == 0) {
+            display.textContent = "Really? Surely you know better.";
+            inp = "";
+            return;
+        } 
         divide(val1, val2);
     } else if (operator == "+") {
         add(val1, val2);
     } else if (operator == "-") {
         subtract(val1, val2);
+    }else if (operator == "**") {
+        exponentiate(val1, val2)
     }};
 
 
 let c = 0
 const add = function(x, y) {
+    if (inp.split(".").length >= 2) {
+        x = parseFloat(x);
+        y = parseFloat(y);
+    } else {
     x = parseInt(x);
     y = parseInt(y);
-	display.textContent = x + y;
-    inp = display.textContent;
+    }
+	inp = x + y;
+    inp = Math.round(inp * 100) / 100;
+    if (isNaN(inp)) {
+        inp = "";
+        display.textContent = "Invalid input."
+        return;
+    }
+    display.textContent = inp;
     
 };
 
 const subtract = function(x, y) {
+    if (inp.split(".").length >= 2) {
+        x = parseFloat(x);
+        y = parseFloat(y);
+    } else {
     x = parseInt(x);
     y = parseInt(y);
-	display.textContent = x - y;
-    inp = display.textContent;
+    }
+	inp = x - y;
+    inp = Math.round(inp * 100) / 100;
+    if (isNaN(inp)) {
+        inp = "";
+        display.textContent = "Invalid input."
+        return;
+    }
+    display.textContent = inp;
 };
 let z = 0
 
 const multiply = function(x, y) {
+    if (inp.split(".").length >= 2) {
+        x = parseFloat(x);
+        y = parseFloat(y);
+    } else {
     x = parseInt(x);
     y = parseInt(y);
+    }
     z = [x, y];
-    display.textContent = z.reduce((a, b) => a * b);
-    inp = display.textContent;
+    inp = z.reduce((a, b) => a * b);
+    inp = Math.round(inp * 100) / 100;
+    if (isNaN(inp)) {
+        inp = "";
+        display.textContent = "Invalid input."
+        return;
+    }
+    display.textContent = inp;
   };
 
 const divide = function(x, y) {
+    if (inp.split(".").length >= 2) {
+        x = parseFloat(x);
+        y = parseFloat(y);
+    } else {
     x = parseInt(x);
     y = parseInt(y);
+    }
     z = [x, y];
-    display.textContent = z.reduce((a, b) => a / b);
-    inp = display.textContent;
+    inp = z.reduce((a, b) => a / b);
+    inp = Math.round(inp * 100) / 100;
+    if (isNaN(inp)) {
+        inp = "";
+        display.textContent = "Invalid input."
+        return;
+    }
+    display.textContent = inp;
 };
 
- 
+const exponentiate = function(x, y) {
+    if (inp.split(".").length >= 2) {
+        x = parseFloat(x);
+        y = parseFloat(y);
+    } else {
+    x = parseInt(x);
+    y = parseInt(y);
+    }
+    z = [x, y];
+    inp = z.reduce((a, b) => a ** b);
+    inp = Math.round(inp * 100) / 100;
+    if (isNaN(inp)) {
+        inp = "";
+        display.textContent = "Invalid input."
+        return;
+    }
+    display.textContent = inp;
+};
